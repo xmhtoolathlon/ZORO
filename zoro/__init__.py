57c7e8ca1f15e5ba0c1723fcb3e1ce78b4c88558
xmhtoolathlon <ygytyygtftytdrfcvgfrteseswaqqw@gmail.com>
2026-02-07 23:32:17 +0800

    Update zoro/__init__.py with FIXME comments

--- zoro/__init__.py
+++ zoro/__init__.py
@@ -1,16 +1,4 @@
-"""
-ZORO - Zero-shot Object Recognition Operations
-A Computer Vision Framework
-"""
+"""ZORO - Zero-shot Object Recognition and Optimization Framework"""
 
 __version__ = "0.1.0"
-
-# TODO: Add lazy imports for heavy modules
-# TODO: Set up logging configuration
-# TODO: Add version compatibility checks
-
-from .core import detector, segmentation
-from .models import backbone
-from .training import trainer
-from .evaluation import metrics
-from .utils import preprocessing
+__author__ = "ZORO Team"
